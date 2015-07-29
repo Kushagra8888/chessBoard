54b19bf508f2547713d8c6ffd9cc9aea9939dcf1
Kushagra8888 <kushagra8888@gmail.com>
2015-07-29 13:46:16 +0530

    implement King

--- src/King.java
+++ src/King.java
@@ -1,5 +1,13 @@
 /**
  * Created by test on 7/29/2015.
  */
-public class King {
+public class King extends ChessPiece {
+    boolean isMoveValid(int xFinal, int yFinal){
+        if ((Math.abs(y - yFinal) == 1) || (Math.abs(x - xFinal) == 1)) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }

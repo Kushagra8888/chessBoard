c013d4f9a895459eb0fbbbcf6e6fb238c440dd84
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-29 13:46:20 +0530

    King implemented

--- src/King.java
+++ src/King.java
@@ -1,5 +1,33 @@
 /**
  * Created by test on 7/29/2015.
  */
-public class King {
+public class King extends ChessPiece{
+
+	King(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		this.isAlive = true;
+	}
+	
+	public boolean isMoveValid(int x, int y)
+	{
+		int diffInX = Math.abs(x - this.x);
+		int diffInY = Math.abs(x - this.y);
+		boolean isMoveValid = (diffInX <= 1) && (diffInY <= 1) && (diffInX + diffInY == 1);
+		return isMoveValid;
+	}
+	
+	public castling(int xRook)
+	{
+		if (xRook == 8)
+		{
+			this.x += 2;
+		}
+		else
+		if (xRook == 1)
+		{
+			this.x -= 2;
+		}
+	}
 }

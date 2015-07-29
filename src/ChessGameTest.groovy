4b530cc2fefc9fbbc583181208e03dd14fce8f17
Kushagra8888 <kushagra8888@gmail.com>
2015-07-29 14:53:20 +0530

    remove test code files

--- src/ChessGameTest.groovy
+++
@@ -1,8 +0,0 @@
-/**
- * Created by test on 7/29/2015.
- */
-class ChessGameTest extends GroovyTestCase {
-    void testMove() {
-
-    }
-}
--- src/PawnTest.groovy
+++
@@ -1,8 +0,0 @@
-/**
- * Created by test on 7/29/2015.
- */
-class PawnTest extends GroovyTestCase {
-    void testIsMoveValid() {
-
-    }
-}

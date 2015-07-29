1e0d7a9cd0af1960f762ec87d4afdf48ec3cec8d
swatianand5693 <swati.anand2011@vit.ac.in>
2015-07-29 13:28:05 +0530

    Bishop isMoveValid function implemented

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,5 +1,17 @@
 /**
  * Created by test on 7/29/2015.
  */
-public class Bishop {
+public class Bishop extends ChessPiece{
+	Bishop(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		this.isAlive = true;
+	}
+	
+	public bool isMoveValid(int x, int y)
+	{
+		bool isMoveValid = x - this.x == y - this.y;
+		return isMoveValid;
+	}
 }

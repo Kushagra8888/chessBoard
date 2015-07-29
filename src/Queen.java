156a5b347df788668eb15e5cac524b84926b6907
Kushagra8888 <kushagra8888@gmail.com>
2015-07-29 13:38:02 +0530

    implement Queen

--- src/Queen.java
+++ src/Queen.java
@@ -1,5 +1,13 @@
 /**
  * Created by test on 7/29/2015.
  */
-public class Queen {
+public class Queen extends ChessPiece {
+    boolean isMoveValid(int xFinal, int yFinal){
+        if (((x == xFinal) || (y == yFinal)) || (Math.abs(x - xFinal) == Math.abs(y - yFinal))) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }

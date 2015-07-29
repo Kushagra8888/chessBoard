8a2aae008a242b4cc12879ed4eedfe06b916050e
Kushagra8888 <kushagra8888@gmail.com>
2015-07-29 13:56:33 +0530

    add constructors for chess pieces

--- src/King.java
+++ src/King.java
@@ -2,6 +2,14 @@
  * Created by test on 7/29/2015.
  */
 public class King extends ChessPiece {
+
+    King(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.isAlive = true;
+    }
+
     boolean isMoveValid(int xFinal, int yFinal){
         if ((Math.abs(y - yFinal) == 1) || (Math.abs(x - xFinal) == 1)) {
             return true;
--- src/Pawn.java
+++ src/Pawn.java
@@ -2,6 +2,13 @@
  * Created by test on 7/29/2015.
  */
 public class Pawn extends ChessPiece {
+    Pawn(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        this.isAlive = true;
+    }
+
     boolean isMoveValid(int xFinal, int yFinal){
         if (y == yFinal) {
             return true;

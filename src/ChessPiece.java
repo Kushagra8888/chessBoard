3bfc397cabf1ecb96610991afee9376f14c5f992
Kushagra8888 <kushagra8888@gmail.com>
2015-07-29 13:24:12 +0530

    implement chessPiece class

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,5 +1,19 @@
 /**
  * Created by test on 7/29/2015.
  */
-public Interface ChessPiece {
+public abstract class ChessPiece {
+    int x, y;
+    boolean isAlive;
+
+    abstract boolean isMoveValid(int x, int y);
+
+    boolean move(int x,int y) {
+        if (isMoveValid(x, y)) {
+            this.x = x;
+            this.y = y;
+            return true;
+        }
+        return false;
+    }
+
 }

7f10a51f31c367408187493141c3fa27a667ef87
Kushagra8888 <kushagra8888@gmail.com>
2015-07-29 13:42:42 +0530

    implement Pawn

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,5 +1,13 @@
 /**
  * Created by test on 7/29/2015.
  */
-public class Pawn {
+public class Pawn extends ChessPiece {
+    boolean isMoveValid(int xFinal, int yFinal){
+        if (y == yFinal) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }

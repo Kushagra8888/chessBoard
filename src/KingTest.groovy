5d5d897790423beae5531ace04bcede4af657ef2
Kushagra8888 <kushagra8888@gmail.com>
2015-07-29 17:02:43 +0530

    add test cases for King

---
+++ src/KingTest.groovy
@@ -0,0 +1,24 @@
+/**
+ * Created by test on 7/29/2015.
+ */
+class KingTest extends GroovyTestCase {
+    void testIsMoveValid() {
+        King aKing = new King(3,4)
+        assert (aKing.isMoveValid(4,4))
+        assert (aKing.isMoveValid(2, 4))
+        assert (aKing.isMoveValid(3,5))
+        assert (aKing.isMoveValid(3,3))
+    }
+
+    void testMove(){
+        King aKing = new King(3,4);
+        assert (aKing.move(4,4))
+        assert aKing.getPosition() == "d4"
+        assert (aKing.move(3,3))
+        assert aKing.getPosition() == "c3"
+    }
+
+    void testCastling() {
+
+    }
+}

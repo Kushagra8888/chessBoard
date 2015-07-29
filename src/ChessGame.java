0489def65c87cc6e6c2861a6bb11e086fe6e4dfd
padmaja20nov <padmaja20nov@gmail.com>
2015-07-29 14:51:02 +0530

    initialisation of chess pieces

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -3,8 +3,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Created by test on 7/29/2015.
+ * 
+ * @TODO
+ * 
+ * 1. Change ArrayList To normal Array in the HashMap
+ * 2. add 'final' member variables for initial positions of various chess pieces
+ *
  */
+
+
 public class ChessGame {
 
 	final int blackPawnInitialYPos = 7;
@@ -26,6 +33,12 @@ public class ChessGame {
 	public ChessGame () {
 		
 		initPawns();
+		initKnight();
+		initRook();
+		initBishop();
+		initKing();
+		initQueen();
+		
 	}
 	
 	private void initPawns () {
@@ -39,6 +52,72 @@ public class ChessGame {
 		
 		blackPieces.put("P", bpawns);
 		whitePieces.put("P", wpawns);
+	}
+	
+	private void initKnight () {
+		ArrayList<ChessPiece> bknights = new ArrayList<ChessPiece>();
+		ArrayList<ChessPiece> wknights = new ArrayList<ChessPiece>();
+		
+		bknights.add(new Knight(2, 8));
+		bknights.add(new Knight(7, 8));
+		
+		wknights.add(new Knight(2, 1));
+		wknights.add(new Knight(7, 1));
+			
+		blackPieces.put("N", bknights);
+		whitePieces.put("N", wknights);
+	}
+	
+	private void initRook () {
+		ArrayList<ChessPiece> brooks = new ArrayList<ChessPiece>();
+		ArrayList<ChessPiece> wrooks = new ArrayList<ChessPiece>();
+		
+		brooks.add(new Rook(1, 8));
+		brooks.add(new Rook(8, 8));
+		
+		wrooks.add(new Rook(1, 1));
+		wrooks.add(new Rook(8, 1));
+			
+		blackPieces.put("R", brooks);
+		whitePieces.put("R", wrooks);
+	}
+	
+	private void initBishop() {
+		ArrayList<ChessPiece> bbishops = new ArrayList<ChessPiece>();
+		ArrayList<ChessPiece> wbishops = new ArrayList<ChessPiece>();
+		
+		bbishops.add(new Bishop(3, 8));
+		bbishops.add(new Bishop(6, 8));
+		
+		wbishops.add(new Bishop(3, 1));
+		wbishops.add(new Bishop(6, 1));
+			
+		blackPieces.put("B", bbishops);
+		whitePieces.put("B", wbishops);
+	}
+	
+	private void initKing() {
+		ArrayList<ChessPiece> bking = new ArrayList<ChessPiece>();
+		ArrayList<ChessPiece> wking = new ArrayList<ChessPiece>();
+		
+		bking.add(new King(5, 8));
+		
+		wking.add(new King(5, 1));
+			
+		blackPieces.put("K", bking);
+		whitePieces.put("K", wking);
+	}
+	
+	private void initQueen() {
+		ArrayList<ChessPiece> bqueen = new ArrayList<ChessPiece>();
+		ArrayList<ChessPiece> wqueen = new ArrayList<ChessPiece>();
+		
+		bqueen.add(new Queen(4, 8));
+		
+		wqueen.add(new Queen(4, 1));
+			
+		blackPieces.put("Q", bqueen);
+		whitePieces.put("Q", wqueen);
 	}
 	
 	public void move (String movePair) {

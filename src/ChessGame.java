b0d4f2b22da930294040712318ae7a4173484c6a
padmaja20nov <padmaja20nov@gmail.com>
2015-07-29 14:26:29 +0530

    API in chessGame class added

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -7,10 +7,46 @@ import java.util.HashMap;
  */
 public class ChessGame {
 
-	static HashMap<String, int[]> coordinateMap = new HashMap<String, int[]>();
-	private HashMap<String, ArrayList<ChessPiece>> chessPieces = new HashMap<String, ArrayList<ChessPiece>>();
+	final int blackPawnInitialYPos = 7;
+	final int whitePawnInitialYPos = 2;
 	
-	public void move (String moveSet) {
-		String[] moves = moveSet.split("\\s+");
+	static HashMap<String, int[]> coordinateMap = new HashMap<String, int[]>();
+	
+	private HashMap<String, ArrayList<ChessPiece>> blackPieces = new HashMap<String, ArrayList<ChessPiece>>();
+	private HashMap<String, ArrayList<ChessPiece>> whitePieces = new HashMap<String, ArrayList<ChessPiece>>();
+	
+	static {
+		generateCoordinateMap();
+	}
+	
+	private static void generateCoordinateMap () {
+		
+	}
+	
+	public ChessGame () {
+		
+		initPawns();
+	}
+	
+	private void initPawns () {
+		ArrayList<ChessPiece> bpawns = new ArrayList<ChessPiece>();
+		ArrayList<ChessPiece> wpawns = new ArrayList<ChessPiece>();
+		
+		for (int i = 1; i <= 8; i++) {
+			bpawns.add(new Pawn(i, blackPawnInitialYPos));
+			wpawns.add(new Pawn(i, whitePawnInitialYPos));
+		}
+		
+		blackPieces.put("P", bpawns);
+		whitePieces.put("P", wpawns);
+	}
+	
+	public void move (String movePair) {
+		String[] moves = movePair.split("\\s+");
+		
+	}
+	
+	public void showBoard () {
+		
 	}
 }

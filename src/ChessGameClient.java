9c9db2b537c0122b9f257e264025b46953552c65
Kushagra8888 <kushagra8888@gmail.com>
2015-07-30 11:06:14 +0530

    fix appending of P for pawns

--- src/ChessGameClient.java
+++ src/ChessGameClient.java
@@ -8,7 +8,7 @@ class ChessGameClient
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		ChessGame chessGame = new ChessGame();
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Adams.pgn"));
+		BufferedReader br = new BufferedReader(new FileReader("Adams.pgn"));
 		String nextMove;
 		String moves = br.readLine();
 		String[] movesArray = moves.split("\\.");
@@ -17,16 +17,17 @@ class ChessGameClient
 		
 		for (int index = 1; index < movesArray.length; index++){
 			currentMove = movesArray[index].split(" ");
-		
-			move = currentMove[0];
-			if	(Character.isUpperCase(currentMove[0].charAt(0)))
+			if(!Character.isUpperCase(currentMove[0].charAt(0)))
 				currentMove[0] = "P" + currentMove[0];
+			move = currentMove[0];
+
 			if (currentMove[1] != null)
 			{	
-				if(Character.isUpperCase(currentMove[1].charAt(0)))
+				if(!Character.isUpperCase(currentMove[1].charAt(0)))
 					currentMove[1] = "P" + currentMove[1];
 				move += " " + currentMove[1];
 			}
+			//System.out.println(move);
 			chessGame.move(move);
 		}
  		br.close();
